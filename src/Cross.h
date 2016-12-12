2fb184f91f9bf00fe65cb62a47a0e1c71632b4ce
Kuco <piotr.kuc29@gmail.com>
2016-12-12 16:33:33 +0100

    Cross  creating neighbourhood  method

--- src/Cross.h
+++ src/Cross.h
@@ -5,9 +5,24 @@
 #ifndef ZPR_CROSS_H
 #define ZPR_CROSS_H
 
+#include <vector>
+#include "Point.h"
 
 class Cross {
 
+    const unsigned int id;
+    std::vector<std::pair<Cross*, Point*> >neighbours;
+    bool visited;
+    Point* position;
+
+public:
+    Cross(const unsigned int&, Point*);
+    std::pair<Cross*, Point*>* getNotVisitedNeighbours();
+    void createNeighbourhood(const std::vector<std::pair<Cross*, Point*>>&);
+    bool setVisited(const bool&);
+    bool getVisited() const;
+    unsigned int getId() const;
+    Point* getPosition() const ;
 };
 
 
